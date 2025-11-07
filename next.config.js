2c1f08c6a96bae562e2e5d57dd23439ed61664e1
piersonshawn99 <pierson.shawn@gmail.com>
2025-11-07 20:51:41 +0000

    added the proper paths.

--- next.config.js
+++ next.config.js
@@ -1,2 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports = { experimental: { optimizePackageImports: ['react'] } };
+module.exports = {
+  experimental: {
+    optimizePackageImports: ['react'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/golf-and-life',
+        destination: '/golf-life',
+        permanent: true,
+      },
+    ];
+  },
+};
